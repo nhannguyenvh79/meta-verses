b01aabc051f46ab9d122e736fa707cfe965bc212
Nguyễn Trọng Nhân <nhannguyenvh79@gmail.com>
2023-09-28 14:04:55 +0700

    add meta tag in head

--- app/head.js
+++ app/head.js
@@ -1,8 +1,25 @@
 const Head = () => (
   <>
     <title>Metaverse</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="title" content="Next Generation Metaverse Website" />
+    <meta
+      name="description"
+      content="Next Generation Metaverse Website -by NhanDev"
+    />
+    <meta name="author" content="NhanDev" />
+    <meta name="googlebot" content="notranslate" />
+    <meta name="rating" content="safe for kids" />
+    <meta name="copyright" content="Copyright 2023" />
+    <meta name="format-detection" content="telephone=yes" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta property="og:title" content="Metaverse" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="/favicon.ico" />
+    <meta property="og:url" content="https://github.com/nhannguyenvh79" />
   </>
 );
 
